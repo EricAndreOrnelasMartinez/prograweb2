b059362e3c81b6726e9339f3b2b95e2b1ee32e04
André Ornelas <ericcot2@gmail.com>
2023-10-14 23:33:48 -0500

    add

--- ExamenP2/backend.php
+++ ExamenP2/backend.php
@@ -22,10 +22,7 @@ if($ans >= 12 && $ans <= 24){
 }
 
 
-$sql = "INSERT INTO test (nombre, email, preg1, preg2, preg3, preg4, preg5, preg6, preg7, preg8
-, preg9, preg10, preg11, preg12, resultado) VALUES ('$name', '$email', $_POST['p1'], $_POST['p2'], 
-$_POST['p3'], $_POST['p4'], $_POST['p5'], $_POST['p6'], $_POST['p7'], $_POST['p8'], $_POST['p9'],
-$_POST['p10'], $_POST['p11'], $_POST['p12'], '$resultado')";
+$sql = "INSERT INTO test (nombre, email, preg1, preg2, preg3, preg4, preg5, preg6, preg7, preg8, preg9, preg10, preg11, preg12, resultado) VALUES ('$name', '$email', $_POST['p1'], $_POST['p2'], $_POST['p3'], $_POST['p4'], $_POST['p5'], $_POST['p6'], $_POST['p7'], $_POST['p8'], $_POST['p9'],$_POST['p10'], $_POST['p11'], $_POST['p12'], '$resultado')";
 echo json_encode($sql);
 /*
 if($conn->query($sql) === TRUE){
