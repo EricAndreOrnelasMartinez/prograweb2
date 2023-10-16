dae4e19b9aab8fb51855869cf9a6fdef09fd9ad8
André Ornelas <ericcot2@gmail.com>
2023-10-16 06:09:57 -0500

    add

--- proyecto5/modificar.php
+++ proyecto5/modificar.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
+$username = "andre";
+$password = "hola";
 $dbname = "mydb1";
 $conn = new mysqli($servername, $username, $password, $dbname);
 $id = $_POST['id'];
