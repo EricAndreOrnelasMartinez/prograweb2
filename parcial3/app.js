74eed57f87aa19547ec62973eb5513b76067665a
André Ornelas <ericcot2@gmail.com>
2023-10-24 06:01:58 -0500

    add

---
+++ parcial3/app.js
@@ -0,0 +1,50 @@
+const form = document.getElementById('info')
+let table = document.getElementById('table')
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    let data = new FormData(form)
+    fetch('backend.php', {
+        method: 'POST',
+        body: data
+    })
+    .then(res => res.json())
+    .then(back =>{
+        let output
+        table.innerHTML = `
+        <tr>
+            <th>ID</th>
+            <th>Matricula</th>
+            <th>Semestre</th>
+            <th>Materia</th>
+            <th>Sección</th>
+            <th>Periodo</th>
+            <th>CFO</th>
+            <th>EXT</th>
+            <th>REG</th>
+            <th>CF</th>
+            <th>Creditos</th>
+            <th>Status</th>
+        </tr>
+        `
+        for(i in back){
+            output += `
+            <tr>
+            <td>${back[i].id}</td>
+            <td>${back[i].matricula}</td>
+            <td>${back[i].semestre}</td>
+            <td>${back[i].materia}</td>
+            <td>${back[i].seccion}</td>
+            <td>${back[i].periodo}</td>
+            <td>${back[i].cfo}</td>
+            <td>${back[i].ext}</td>
+            <td>${back[i].reg}</td>
+            <td>${back[i].cf}</td>
+            <td>${back[i].creditos}</td>
+            <td>${back[i].status}</td>
+            </tr>
+            `
+        }
+        table.innerHTML += output
+    })
+})
---
+++ parcial3/backend.php
@@ -0,0 +1,33 @@
+<?php
+require_once('dbcon.php');
+$opc = $_POST['opc'];
+if($opc === 'opc1'){
+    $sql = "SELECT * FROM `kardex` WHERE `semestre`=3";
+    $res = $con->query($sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc2'){
+    $sql = "SELECT * FROM `kardex` WHERE `status`='Aprobado'";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc2'){
+    $sql = "SELECT * FROM `kardex` WHERE `status`='-'";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc3'){
+    $sql = "SELECT * FROM `kardex` WHERE `status`='-'";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc4'){
+    $sql = "SELECT * FROM `kardex` WHERE `status`='-' AND `periodo` ='20233S'";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc5'){
+    $sql = "SELECT sum(`creditos`) from kardex";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc5'){
+    $sql = "SELECT avg(`cf`) from kardex where `periodo` != '' and `status` != '-'";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}
+?>
