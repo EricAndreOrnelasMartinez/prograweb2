1478c36e990a4899bb51f5911dccbb090e69f993
Eric <56614110+EricAndreOrnelasMartinez@users.noreply.github.com>
2023-10-24 09:43:04 -0600

    Add files via upload
    add

--- parcial3/app.js
+++ parcial3/app.js
@@ -1,4 +1,5 @@
 const form = document.getElementById('info')
+const table = documet.getElementById('table')
 
 form.addEventListener('submit', e =>{
     e.preventDefault()
@@ -9,6 +10,39 @@ form.addEventListener('submit', e =>{
     })
     .then(res => res.json())
     .then(back =>{
-        console.log(back)
+        let output
+        table.innerHTML = `
+        <tr>
+            <th>ID</th>
+            <th>Matricula</th>
+            <th>Semestre</th>
+            <th>Materia</th>
+            <th>Sección</th>
+            <th>Periodo</th>
+            <th>CFO</th>
+            <th>EXT</th>
+            <th>REG</th>
+            <th>CF</th>
+            <th>Creditos</th>
+            <th>Status</th>
+        </tr>
+        `
+        for(i in back){
+            output += `
+            <tr>${back[i].id}</tr>
+            <tr>${back[i].matricula}</tr>
+            <tr>${back[i].semestre}</tr>
+            <tr>${back[i].materia}</tr>
+            <tr>${back[i].seccion}</tr>
+            <tr>${back[i].periodo}</tr>
+            <tr>${back[i].cfo}</tr>
+            <tr>${back[i].ext}</tr>
+            <tr>${back[i].reg}</tr>
+            <tr>${back[i].cf}</tr>
+            <tr>${back[i].creditos}</tr>
+            <tr>${back[i].status}</tr>
+            `
+        }
+        table.innerHTML += output
     })
 })
