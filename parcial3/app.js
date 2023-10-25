6a5d9da486dec95e2d803b03ead2d2bfa52f9451
André Ornelas <ericcot2@gmail.com>
2023-10-25 07:40:06 -0500

    add

--- parcial3/app.js
+++ parcial3/app.js
@@ -11,41 +11,54 @@ form.addEventListener('submit', e =>{
     })
     .then(res => res.json())
     .then(back =>{
-        let output
-        table.innerHTML = `
-        <tr>
-            <th>ID</th>
-            <th>Matricula</th>
-            <th>Semestre</th>
-            <th>Materia</th>
-            <th>Sección</th>
-            <th>Periodo</th>
-            <th>CFO</th>
-            <th>EXT</th>
-            <th>REG</th>
-            <th>CF</th>
-            <th>Creditos</th>
-            <th>Status</th>
-        </tr>
-        `
-        for(i in back){
-            output += `
+        console.log(back)
+        console.log(typeof(back))
+        if(back.length){
+            let output
+            table.innerHTML = `
             <tr>
-            <td>${back[i].id}</td>
-            <td>${back[i].matricula}</td>
-            <td>${back[i].semestre}</td>
-            <td>${back[i].materia}</td>
-            <td>${back[i].seccion}</td>
-            <td>${back[i].periodo}</td>
-            <td>${back[i].cfo}</td>
-            <td>${back[i].ext}</td>
-            <td>${back[i].reg}</td>
-            <td>${back[i].cf}</td>
-            <td>${back[i].creditos}</td>
-            <td>${back[i].status}</td>
+                <th>ID</th>
+                <th>Matricula</th>
+                <th>Semestre</th>
+                <th>Materia</th>
+                <th>Sección</th>
+                <th>Periodo</th>
+                <th>CFO</th>
+                <th>EXT</th>
+                <th>REG</th>
+                <th>CF</th>
+                <th>Creditos</th>
+                <th>Status</th>
+            </tr>
+            `
+            for(i in back){
+                output += `
+                <tr>
+                <td>${back[i].id}</td>
+                <td>${back[i].matricula}</td>
+                <td>${back[i].semestre}</td>
+                <td>${back[i].materia}</td>
+                <td>${back[i].seccion}</td>
+                <td>${back[i].periodo}</td>
+                <td>${back[i].cfo}</td>
+                <td>${back[i].ext}</td>
+                <td>${back[i].reg}</td>
+                <td>${back[i].cf}</td>
+                <td>${back[i].creditos}</td>
+                <td>${back[i].status}</td>
+                </tr>
+                `
+            }
+            table.innerHTML += output
+        }else{
+            table.innerHTML = `
+            <tr>
+                <td>Valor</td>
+            </tr>
+            <tr>
+                <td>${back}</td>
             </tr>
             `
         }
-        table.innerHTML += output
     })
 })
