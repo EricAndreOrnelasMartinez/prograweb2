fd9176e78a6528cc65714d36eefe35fa12000b1c
Eric <56614110+EricAndreOrnelasMartinez@users.noreply.github.com>
2023-10-25 09:19:11 -0600

    add

--- parcial3/backend.php
+++ parcial3/backend.php
@@ -9,23 +9,23 @@ if($opc === 'opc1'){
     $sql = "SELECT * FROM `kardex` WHERE `status`='Aprobado'";
     $res = mysqli_query($con, $sql);
     echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
-}else if($opc === 'opc2'){
-    $sql = "SELECT * FROM `kardex` WHERE `status`='-'";
-    $res = mysqli_query($con, $sql);
-    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
 }else if($opc === 'opc3'){
     $sql = "SELECT * FROM `kardex` WHERE `status`='-'";
     $res = mysqli_query($con, $sql);
     echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
 }else if($opc === 'opc4'){
+    $sql = "SELECT * FROM `kardex` WHERE `status`='-'";
+    $res = mysqli_query($con, $sql);
+    echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
+}else if($opc === 'opc5'){
     $sql = "SELECT * FROM `kardex` WHERE `status`='-' AND `periodo` ='20233S'";
     $res = mysqli_query($con, $sql);
     echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
-}else if($opc === 'opc5'){
+}else if($opc === 'opc6'){
     $sql = "SELECT sum(`creditos`) from kardex";
     $res = mysqli_query($con, $sql);
     echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
-}else if($opc === 'opc5'){
+}else if($opc === 'opc7'){
     $sql = "SELECT avg(`cf`) from kardex where `periodo` != '' and `status` != '-'";
     $res = mysqli_query($con, $sql);
     echo json_encode(mysqli_fetch_all($res, MYSQLI_ASSOC));
