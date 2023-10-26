af22d36913ef733523d2e05b697b936c59016209
EricAndreOrnelasMartinez <ericcot2@gmail.com>
2023-10-26 09:52:47 -0600

    add

---
+++ ParcialN/Ejercicio2/ejercicio3.php
@@ -0,0 +1,16 @@
+<html>
+<head><title>HOME</title>
+</head>
+<body>
+<h1>Contador con Sesiones</h1>
+<?php
+session_start();
+if(!isset($_SESSION['contador']))
+ $_SESSION['contador']=0;
+else $_SESSION['contador']++;
+echo "Visitas:".$_SESSION['contador'];
+?>
+<br><a href="reiniciaCont.php">Reiniciar contador</a>
+</body>
+</html>
+
