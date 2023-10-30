dd84393457ef28f5e93c66f8a0c4cbae93bd4c17
EricAndreOrnelasMartinez <ericcot2@gmail.com>
2023-10-30 09:46:50 -0600

    add

--- ParcialN/Ejercicio1/backend.php
+++ ParcialN/Ejercicio1/backend.php
@@ -5,6 +5,8 @@ $contra = $_POST['contra'];
 $sql = "SELECT * FROM usuarios WHERE usuario='$usuario'";
 $res = $con->query($sql);
 if($res->num_rows > 0){
+    session_start();
+    $_SESSION['usuario'] = $res['usuario'];
     echo json_encode('1');
 }else{
     echo json_encode('0');
