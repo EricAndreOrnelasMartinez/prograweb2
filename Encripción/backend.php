35dc0e47d13510b921b778238935f23adbd8ba27
Eric <56614110+EricAndreOrnelasMartinez@users.noreply.github.com>
2023-11-13 09:35:06 -0600

    Add files via upload

--- Encripción/backend.php
+++ Encripción/backend.php
@@ -4,11 +4,11 @@ $user = $_POST['user'];
 $password = $_POST['password'];
 $aux = md5($password);
 $name = $_POST['name'];
-$email = $_POST['email'];;
-$sql = "INSERT INTO usarios (usuario,password,nombre,email,tipo) values('$user','$aux','$name',$email,1)";
+$email = $_POST['email'];
+$sql = "INSERT INTO usarios (usuario,password,nombre,email,tipo) values('$user','$aux','$name','$email',1)";
 if($con->query($sql) === TRUE){
-    echo json_encode("1");
+    echo json_encode('1');
 }else{
-    echo json_encode("0");
+    echo json_encode('0');
 }
 ?>
