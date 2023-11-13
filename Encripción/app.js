906e40591b709f546e27db9e14958b979fac0f31
Eric <56614110+EricAndreOrnelasMartinez@users.noreply.github.com>
2023-11-13 09:30:07 -0600

    Add files via upload

---
+++ Encripción/app.js
@@ -0,0 +1,18 @@
+const form = document.getElementById('info');
+
+form.addEventListener('submit', e=>{
+    e.preventDefault()
+    let data = new FormData(form)
+    fetch('./backend.php', {
+        method: 'POST',
+        body: data
+    })
+    .then(res => res.json())
+    .then(back =>{
+        if(back == '1'){
+            console.log('Registrado');
+        }else{
+            console.log('error 500')
+        }
+    })
+})
---
+++ Encripción/backend.php
@@ -0,0 +1,14 @@
+<?php
+require_once('dbcon.php');
+$user = $_POST['user'];
+$password = $_POST['password'];
+$aux = md5($password);
+$name = $_POST['name'];
+$email = $_POST['email'];;
+$sql = "INSERT INTO usarios (usuario,password,nombre,email,tipo) values('$user','$aux','$name',$email,1)";
+if($con->query($sql) === TRUE){
+    echo json_encode("1");
+}else{
+    echo json_encode("0");
+}
+?>
