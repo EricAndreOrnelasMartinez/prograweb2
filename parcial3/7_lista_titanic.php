5a6ec10455097b08ae70b2cfd5a14d98893a856e
André Ornelas <ericcot2@gmail.com>
2023-11-20 08:19:42 -0600

    add

--- parcial3/7_lista_titanic.php
+++ parcial3/7_lista_titanic.php
@@ -2,11 +2,8 @@
 require('fpdf.php');
 require('conexion.php');
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
 //Check connection
-if ($conn->connect_error){
-die("Connection failed: ".$conn->connect_error);
-}
+
 $sql = "SELECT name FROM registro WHERE survived=1 ";
 $result = $conn->query($sql);
 if ($result->num_rows > 0){
