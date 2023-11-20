e7cb86d916039bf77d60cbda07c85cc015e0d36c
André Ornelas <ericcot2@gmail.com>
2023-11-20 01:57:31 -0600

    add

--- ExamenPractico3/registro.js
+++ ExamenPractico3/registro.js
@@ -1,5 +1,5 @@
 const form = document.getElementById('info')
-
+const h3 = document.getElementById('res')
 form.addEventListener('submit', e=>{
     e.preventDefault()
     let data = new FormData(form)
@@ -9,6 +9,14 @@ form.addEventListener('submit', e=>{
     })
     .then(res => res.json())
     .then(back =>{
-        console.log(back)
+        if(back == '2'){
+            h3.innerHTML = 'Correo existente'
+            h3.style = 'background-color: red'
+        }else if(back == 1){
+            window.location.assign('./index.html')
+        }else{
+            h3.innerHTML = 'ERROR 500'
+            h3.style = 'background-color: red'           
+        }
     })
 })
