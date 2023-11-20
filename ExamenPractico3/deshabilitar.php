777a5d64eee25ab6927078e8017d618435ef1eb4
André Ornelas <ericcot2@gmail.com>
2023-11-20 01:10:48 -0600

    add

--- ExamenPractico3/deshabilitar.php
+++ ExamenPractico3/deshabilitar.php
@@ -5,3 +5,14 @@ $sql = "UPDATE enable set enable='0' where email='$email'";
 $conn->query($sql);
 header('./main.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
--- ExamenPractico3/habilitar.php
+++ ExamenPractico3/habilitar.php
@@ -2,6 +2,17 @@
 require('dbcon.php');
 $email = $_GET['email'];
 $sql = "UPDATE enable set enable='1' where email='$email'";
-$conn->query($sql)
+$conn->query($sql);
 header('./main.php');
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
